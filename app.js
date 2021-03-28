9989bd260aac9ab2025e4ada3b6689ef6f03ca21
creditFool <bichlasulbulqiah@gmail.com>
2021-03-28 08:36:44 +0700

    Menambah fitur mengganti pesan

--- app.js
+++ app.js
@@ -4,9 +4,27 @@ window.onload = displayClock();
 
 let panelVisible = false;
 
+var timeToChange = 3;
+let pesan = [
+    "Happy",
+    "Lucky",
+    "Smile",
+    "Yay!",
+    "Kokoro Best Girl UwU"
+];
+var timer = 0;
+let pesanSaatIni = 0;
+
 function displayClock() {
     let d = new Date().toLocaleTimeString();
     document.getElementById("waktu").innerHTML = d;
+
+    if (timer++ == timeToChange) {
+        document.getElementById("pesan-utama").innerHTML = pesan[pesanSaatIni++];
+        if (pesanSaatIni == pesan.length) pesanSaatIni = 0;
+        timer = 0;
+    }
+
     setTimeout(displayClock, 1000);
 }
 
