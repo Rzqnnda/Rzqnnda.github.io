311ecee05699d408c0bb7e79ed517b68e4edf8e7
creditFool <bichlasulbulqiah@gmail.com>
2021-03-28 08:16:40 +0700

    menambah fitur tombol sidebar

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ console.log("Hello, Happy World!");
 
 window.onload = displayClock();
 
+let panelVisible = false;
+
 function displayClock() {
     let d = new Date().toLocaleTimeString();
     document.getElementById("waktu").innerHTML = d;
     setTimeout(displayClock, 1000);
+}
+
+function klik() {
+    document.getElementById("panel-base").style.visibility = (!panelVisible ? "visible" : "hidden");
+    panelVisible = !panelVisible;
 }
